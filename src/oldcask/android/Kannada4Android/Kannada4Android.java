49cb1454a89a7e132e3c98906ede4dde96c710b5
HARIHARAN <hariharanwebmail@gmail.com>
2010-12-16 19:04:42 +0530

    First Kannada4Android push.... <oldcask>

---
+++ src/oldcask/android/Kannada4Android/Kannada4Android.java
@@ -0,0 +1,13 @@
+package oldcask.android.Kannada4Android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Kannada4Android extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
