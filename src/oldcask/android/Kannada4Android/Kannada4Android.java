ba07aceb7e6b235b008308dc50d2f2f7f72d0914
HARIHARAN <harihat@thoughtworks.com>
2010-12-20 23:03:20 +0530

    Added code to call OCR async <hari>

--- src/oldcask/android/Kannada4Android/Kannada4Android.java
+++ src/oldcask/android/Kannada4Android/Kannada4Android.java
@@ -6,13 +6,30 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class Kannada4Android extends Activity {
-    /** Called when the activity is first created. */
+    private final IOpticalCharacterRecognizer ocr;
+
+    public Kannada4Android() {
+    	this(new OpticalCharacterRecognizer());
+	}
+    
+	public Kannada4Android(IOpticalCharacterRecognizer ocr) {
+		this.ocr = ocr;
+	}
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        IOpticalCharacterRecognizer ocr = new OpticalCharacterRecognizer();
-        ocr.recognize(null);
-        System.out.println("Done here!!");
+        trainTheNetwork();
     }
+
+	private void trainTheNetwork() {
+		Thread trainerThread = new Thread(new Runnable() {
+			public void run() {
+				ocr.trainNetwork();
+			}
+		});
+		trainerThread.start();
+	}
 }
