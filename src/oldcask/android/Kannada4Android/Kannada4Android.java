1b1e6111fb0e0eb0b15adab34f96d67b4f7b8ab1
HARIHARAN <harihat@thoughtworks.com>
2010-12-24 23:18:55 +0530

    Async Call to recognizer when clicked. <hari>

--- src/oldcask/android/Kannada4Android/Kannada4Android.java
+++ src/oldcask/android/Kannada4Android/Kannada4Android.java
@@ -1,7 +1,7 @@
 package oldcask.android.Kannada4Android;
 
 import oldcask.android.Kannada4Android.interfaces.IOpticalCharacterRecognizer;
-import oldcask.android.Kannada4Android.ocr.OpticalCharacterRecognizer;
+import oldcask.android.Kannada4Android.interfaces.OpticalCharacterRecognizerFactory;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -10,25 +10,26 @@ import android.view.View;
 import android.widget.Button;
 
 public class Kannada4Android extends Activity {
-    private final IOpticalCharacterRecognizer ocr;
-	
-    public Kannada4Android() {
-    	this(new OpticalCharacterRecognizer());
+	private final IOpticalCharacterRecognizer ocr;
+
+	public Kannada4Android() {
+		this(OpticalCharacterRecognizerFactory.getOpticalCharacterRecognizer());
 	}
-    
+
 	public Kannada4Android(IOpticalCharacterRecognizer ocr) {
 		this.ocr = ocr;
 	}
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);        
-        trainTheNetwork();
-    }
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		trainTheNetwork();
+	}
 
 	private void trainTheNetwork() {
-		AsyncTask<IOpticalCharacterRecognizer, Integer, Integer> trainerTask = new TrainerDataLoader(this);
+		AsyncTask<IOpticalCharacterRecognizer, Integer, Integer> trainerTask = new TrainerDataLoader(
+				this);
 		trainerTask.execute(ocr);
 	}
 
@@ -36,11 +37,12 @@ public class Kannada4Android extends Activity {
 		Button continueButton = (Button) findViewById(R.id.ContinueButton);
 		continueButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				Intent intent = new Intent(getBaseContext(), CameraActivity.class);
+				Intent intent = new Intent(getBaseContext(),
+						CameraActivity.class);
 				startActivity(intent);
 			}
 		});
-		
+
 		continueButton.setVisibility(View.VISIBLE);
 	}
 }
