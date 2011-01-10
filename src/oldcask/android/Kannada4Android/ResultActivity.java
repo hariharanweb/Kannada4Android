e6d8b386a864ef8855aac5dabab00f335f6d74b5
HARIHARAN <hariharanwebmail@gmail.com>
2011-01-10 11:43:03 +0530

    Text 2 speech part1 - <antony/hari>

--- src/oldcask/android/Kannada4Android/ResultActivity.java
+++ src/oldcask/android/Kannada4Android/ResultActivity.java
@@ -5,17 +5,26 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ResultActivity extends Activity {
-
+	
 	private static final String OCR_RESULT = "OCR_RESULT";
+	private TextToSpeech textToSpeech;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.result);
 		OCRResult result = getData();
 		showResult(result);
+		final Button speakButton = (Button) findViewById(R.id.SpeakOut);
+		OnInitListener ttsListener = new TextToSpeechInitListener(speakButton);
+		textToSpeech = new TextToSpeech(getApplicationContext(), ttsListener);
+		speakButton.setOnClickListener(new SpeakOutClickListener());
 	}
 
 	private OCRResult getData() {
@@ -35,4 +44,25 @@ public class ResultActivity extends Activity {
 		TextView literalTranslation = (TextView) findViewById(R.id.translation);
 		literalTranslation.setText(result.getLiteralTranslation());
 	}
+	
+	private final class SpeakOutClickListener implements View.OnClickListener {
+		public void onClick(View v) {
+			TextView literalTranslation = (TextView) findViewById(R.id.translation);
+			textToSpeech.speak(literalTranslation.getText().toString(),
+					TextToSpeech.QUEUE_ADD, null);
+		}
+	}
+
+	private final class TextToSpeechInitListener implements OnInitListener {
+		private final Button speakButton;
+
+		private TextToSpeechInitListener(Button speakButton) {
+			this.speakButton = speakButton;
+		}
+
+		public void onInit(int status) {
+			speakButton.setVisibility(View.VISIBLE);
+		}
+	}
+
 }
